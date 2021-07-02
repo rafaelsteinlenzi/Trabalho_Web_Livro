c600e79cc55e6cc93b222106d85ebb9c7c3ad3cb
rafaelsteinlenzi <69546566+rafaelsteinlenzi@users.noreply.github.com>
2021-07-01 21:41:06 -0300

    Add files via upload

---
+++ AulaWeb/backend/server/route/book/booksRoute.js
@@ -0,0 +1,29 @@
+const { Router } = require('express');
+const express = require('express');
+const booksService = require('../../service/book/booksService');
+const router = express();
+
+//retorna todos os livros
+router.get('/books', async function(req, res) {
+    const books = await booksService.getBooks();
+    res.json(books);
+});
+
+//retorna livro por codigo passado na URL
+router.get('/book/:codigo', async function(req, res) {
+   const book = await booksService.getBook(req.params.codigo);
+   res.json(book);
+});
+
+router.delete('/book/:codigo', async function(req, res) {
+    await booksService.deleteBook(req.params.codigo);
+    return res.json([{message: 'registro excluido com sucesso'}])
+});
+
+router.put('/book', async function(req, res) {
+    const book = req.body;
+    const newBook = await booksService.saveBook(book);
+    res.json(newBook)
+});
+
+module.exports = router;
